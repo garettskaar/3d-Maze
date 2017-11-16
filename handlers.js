de02150cb5d3df81c612628de0b3d59b395f59a7
Jordin Rausch <jordin.rausch@gmail.com>
2017-11-16 15:19:15 -0700

    rollin rollin rollin

---
+++ handlers.js
@@ -0,0 +1,103 @@
+var mouseDown = false;
+var lastMouseX = null;
+var lastMouseY = null;
+var currentlyPressedKeys = {};
+var zoom = -3;
+var shift = 0;
+var raise = 0;
+
+var pitch = 0;
+var pitchRate = 0;
+var yaw = 0;
+var yawRate = 0;
+var xPos = 0;
+var yPos = 1.0;
+var zPos = 0;
+var speed = 0;
+
+var rotMatrix = mat4.create();
+mat4.identity(rotMatrix);
+
+function degToRad(deg) {
+    return (deg * Math.PI) / 180;
+}
+
+/**
+var handleMouseDown = function (event) {
+    mouseDown = true;
+    lastMouseX = event.clientX;
+    lastMouseY = event.clientY;
+};
+
+var handleMouseUp = function (event) {
+    mouseDown = false;
+};
+
+var handleMouseMove = function (event) {
+    if(!mouseDown) {
+        return;
+    }
+
+    var newX = event.clientX;
+    var newY = event.clientY;
+
+    var diffX = newX - lastMouseX;
+    yaw += diffX;
+    //var newRotMatrix = mat4.create();
+    //mat4.identity(newRotMatrix);
+    //mat4.rotate(newRotMatrix, degToRad(diffX/4), [0, -1, 0]);
+
+    var diffY = newY - lastMouseY;
+    console.log(diffY);
+    speed -= diffY/10000000;
+
+    //mat4.translate(newRotMatrix, [0, 0, .1 * (diffY/10)]);
+    //mat4.multiply(newRotMatrix, rotMatrix, rotMatrix);
+
+    lastMouseX = newX;
+    lastMouseY = newY;
+};
+**/
+
+
+function handleKeyDown(event) {
+    currentlyPressedKeys[event.keyCode] = true;
+}
+
+function handleKeyUp(event) {
+    currentlyPressedKeys[event.keyCode] = false;
+}
+
+function handleKeys() {
+
+    if (currentlyPressedKeys[40]) {
+        // W
+        pitchRate += 0.1;
+    } else if (currentlyPressedKeys[38]) {
+        // D
+        pitchRate += -0.1;
+    } else if(pitchRate < 0) {
+        pitchRate += 0.1
+    } else if(pitchRate > 0) {
+        pitchRate += -0.1
+    } else {
+        pitchRate = 0;
+    }
+
+    if (currentlyPressedKeys[39]) {
+        // Left cursor key
+        yawRate += 0.1;
+    } else if (currentlyPressedKeys[37]) {
+        // Right cursor key
+        yawRate += -0.1;
+    } else {
+        yawRate = 0;
+    }
+    if (currentlyPressedKeys[83]) {
+        // Up cursor key
+        speed = 0.003;
+    } else if (currentlyPressedKeys[87]) {
+        // Down cursor key
+        speed = -0.003;
+    }
+}
