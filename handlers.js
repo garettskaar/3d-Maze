6f8dd512ebff92a1dc00fdee90d4c7a54e8df126
garettskaar <skaargarett@gmail.com>
2017-12-07 11:02:17 -0700

    ugh a little cleaner

--- handlers.js
+++ handlers.js
@@ -8,11 +8,11 @@ var raise = 0;
 
 var pitch = 0;
 var pitchRate = 0;
-var yaw = 0;
+var yaw = 90;
 var yawRate = 0;
-var xPos = 0;
-var yPos = 0;
-var zPos = 15;
+var xPos = 5.4;
+var yPos = 0.3;
+var zPos = 4.0;
 var speed = 0;
 
 var rotMatrix = mat4.create();
@@ -67,41 +67,73 @@ function handleKeyDown(event) {
 function handleKeyUp(event) {
     currentlyPressedKeys[event.keyCode] = false;
 }
-
 function handleKeys() {
-
-    if (currentlyPressedKeys[87]) {
-        // W
-        pitchRate += 0.05;
-    } else if (currentlyPressedKeys[83]) {
-        // D
-        pitchRate += -0.05;
-    } else if(pitchRate < -0.1) {
-        pitchRate += 0.1;
-    } else if(pitchRate > 0.1) {
-        pitchRate += -0.1;
+    if (currentlyPressedKeys[38]) {
+        // Up
+        pitchRate = 0.1;
+    } else if (currentlyPressedKeys[40]) {
+        // Down
+        pitchRate = -0.1;
     } else {
         pitchRate = 0;
     }
 
-    if (currentlyPressedKeys[65]) {
-        // Left cursor key
-        yawRate += 0.05;
-    } else if (currentlyPressedKeys[68]) {
-        // Right cursor key
-        yawRate += -0.05;
-    } else if(yawRate < -0.1) {
-        yawRate += 0.1;
-    } else if(yawRate > 0.1) {
-        yawRate += -0.1;
+    if (currentlyPressedKeys[37] || currentlyPressedKeys[65]) {
+        // Left cursor key or A
+        yawRate = 0.2;
+    } else if (currentlyPressedKeys[39] || currentlyPressedKeys[68]) {
+        // Right cursor key or D
+        yawRate = -0.2;
     } else {
         yawRate = 0;
     }
-    if (currentlyPressedKeys[83]) {
-        // Up cursor key
-        speed = 0.0003;
-    } else if (currentlyPressedKeys[87]) {
-        // Down cursor key
-        speed = -0.0003;
+
+    if (currentlyPressedKeys[87]) {
+        // W // 38
+        speed = 0.003;
+    } else if (currentlyPressedKeys[83]) {
+        // S // 40
+        speed = -0.003;
+    } else {
+        speed = 0;
     }
+
 }
+
+// function handleKeys() {
+//
+//     if (currentlyPressedKeys[87]) {
+//         // W
+//         pitchRate += 0.05;
+//     } else if (currentlyPressedKeys[83]) {
+//         // D
+//         pitchRate += -0.05;
+//     } else if(pitchRate < -0.1) {
+//         pitchRate += 0.1;
+//     } else if(pitchRate > 0.1) {
+//         pitchRate += -0.1;
+//     } else {
+//         pitchRate = 0;
+//     }
+//
+//     if (currentlyPressedKeys[65]) {
+//         // Left cursor key
+//         yawRate += 0.05;
+//     } else if (currentlyPressedKeys[68]) {
+//         // Right cursor key
+//         yawRate += -0.05;
+//     } else if(yawRate < -0.1) {
+//         yawRate += 0.1;
+//     } else if(yawRate > 0.1) {
+//         yawRate += -0.1;
+//     } else {
+//         yawRate = 0;
+//     }
+//     if (currentlyPressedKeys[83]) {
+//         // Up cursor key
+//         speed = 0.0003;
+//     } else if (currentlyPressedKeys[87]) {
+//         // Down cursor key
+//         speed = -0.0003;
+//     }
+// }
